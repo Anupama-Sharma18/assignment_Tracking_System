b8239280de8fa426e8c60b468962727b3fc8631d
Anupama-Sharma18 <anusharma0418@gmail.com>
2026-08-21 14:19:07 +0530

    first commit

---
+++ src/main/java/com/example/assignment_Tracking_System/AssignmentTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_Tracking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentTrackingSystemApplication.class, args);
+	}
+
+}
