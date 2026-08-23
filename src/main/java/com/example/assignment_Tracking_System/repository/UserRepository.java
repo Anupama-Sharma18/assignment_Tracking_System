372228c4eb3651f4e954e8a226030152b59d2ccd
Anupama-Sharma18 <anusharma0418@gmail.com>
2026-08-23 11:29:29 +0530

    made student controller, service, repository and  user and submission service

--- src/main/java/com/example/assignment_Tracking_System/repository/UserRepository.java
+++ src/main/java/com/example/assignment_Tracking_System/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.assignment_Tracking_System.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
